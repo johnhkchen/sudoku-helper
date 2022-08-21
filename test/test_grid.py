283978ba75edaf94fb28a56e1bc6960e7baad2ac
John Chen <john.hk.chen@gmail.com>
2022-08-21 11:00:20 -0700

    Refactor test_grid.py

--- test/test_grid.py
+++ test/test_grid.py
@@ -49,29 +49,28 @@ def test_grid_get_coords(easy_numbers, easy_puzzle):
             coords.append((x, y))
             answer.append(easy_numbers[y][x])
     query = easy_puzzle.get_coords(coords)
-    assert all([ans == elem for ans, elem in zip(answer, query)])
+    assert answer == query
 
 
 def test_grid_get_row(easy_numbers, easy_puzzle):
     for y in range(9):
         answer = [easy_numbers[y][x] for x in range(9)]
         query = easy_puzzle.get_row(y)
-        assert all([ans == elem for ans, elem in zip(answer, query)])
+        assert answer == query
 
 
 def test_grid_get_col(easy_numbers, easy_puzzle):
     for x in range(9):
         answer = [easy_numbers[y][x] for y in range(9)]
         query = easy_puzzle.get_col(x)
-        assert all([ans == elem for ans, elem in zip(answer, query)])
+        assert answer == query
 
 
 def test_grid_get_sector(easy_numbers, easy_puzzle):
     # Sector can also be queried using x, y coords to mimic row/col behavior
     answer = [easy_numbers[y][x] for x, y in get_sector_coords(0, 0)]
     query = easy_puzzle.get_sector(0, 0)
-    assert len(query) == len(answer)
-    assert all([ans == elem for ans, elem in zip(answer, query)])
+    assert answer == query
 
 
 def test_grid_set(easy_numbers, easy_puzzle):
@@ -82,6 +81,7 @@ def test_grid_set(easy_numbers, easy_puzzle):
 
 
 def test_candidates_matrix_exists(easy_puzzle):
+    # Trying to access the candidates matrix should not lead to error
     assert easy_puzzle.candidates is not None
     assert len(easy_puzzle.candidates) == 9
     for y in range(9):
@@ -113,16 +113,11 @@ def test_update_candidates(easy_puzzle):
 
 
 def test_iterate_solving(easy_puzzle):
-    # Single-candidates solvable on first iteration:
-    # (0, 0) -> 7
-    # (0, 1) -> 1
-    # (0, 2) -> 4
+    # iterate_solving() should fill in solutions with immediate solutions
     coords = [(0, 0), (0, 1), (0, 2)]
-    answer = [0, 0, 0]
     query = easy_puzzle.get_coords(coords)
-    assert all([ans == elem for ans, elem in zip(answer, query)])
+    assert [0, 0, 0] == query
 
     easy_puzzle.iterate_solving()
-    answer = [7, 1, 4]
     query = easy_puzzle.get_coords(coords)
-    assert all([ans == elem for ans, elem in zip(answer, query)])
+    assert [7, 1, 4] == query
